f124bf9969a033a1d15f4ae5ed8b6ea993f9c14f
Virgilio Padron Batun <virgilio.padron.sop.j@gmail.com>
2020-07-26 09:57:06 -0500

    Algo esta mal, no esta funcionando

--- app/src/main/java/com/genericsl/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/genericsl/presenter/RegisterPresenter.kt
@@ -4,10 +4,13 @@ import com.genericsl.interactor.clientRest.Register.RegisterServiceImp
 import com.genericsl.interactor.models.RegisterSuccess
 import com.genericsl.interactor.models.User
 import com.genericsl.view.ui.activity.register.IRegisterView
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import org.koin.core.parameter.parametersOf
 
-class RegisterPresenter(val view: IRegisterView):IRegisterPresenter {
+class RegisterPresenter(val view: IRegisterView):IRegisterPresenter, KoinComponent {
 
-    val interactor: RegisterServiceImp = RegisterServiceImp(this)
+    val interactor:RegisterServiceImp by inject{ parametersOf(this)  }
 
     override fun onRegister(user: User) {
         interactor.getRegister(user)
--- app/src/main/java/com/genericsl/view/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/com/genericsl/view/ui/activity/login/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import com.genericsl.R
 import com.genericsl.interactor.models.Login
